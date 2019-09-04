4d9f39bccc272e8bc21a5fea97b4881e324f1c7d
abhi66 <abhi66@rocketmail.com>
2019-09-04 14:48:19 +0530

    Exercise Completed

---
+++ battle-tank-game/Assets/Prefabs/Tank/TankController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController  {
+    public TankModel TankModel { get; }
+    public TankView TankView { get; }
+
+    public TankController(TankModel tankModel, TankView tankPrefab)
+    {
+        TankModel = tankModel;
+        TankView = GameObject.Instantiate<TankView>(tankPrefab);
+        Debug.Log("tankview created",TankView);
+    }
+	
+	
+}
---
+++ battle-tank-game/Assets/Prefabs/Tank/TankService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonoBehaviour {
+
+   public TankView tankView;
+  
+    void Start () {
+        CreateTank(new Vector3(0, 0, 0));
+        CreateTank(new Vector3(0, 0, -4));
+        CreateTank(new Vector3(5, 0, -2));
+    }
+
+
+    void CreateTank(Vector3 transform){
+    TankModel tankModel = new TankModel(10, 100f);
+        tankView.GetComponent<Transform>().position = transform;
+    TankController tank = new TankController(tankModel, tankView);
+}
+	
+}
